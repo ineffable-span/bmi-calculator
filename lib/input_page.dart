8629a85789c52a989011a284bf17e9edf2625aa5
m-zaink <sidsmith9@gmail.com>
2019-06-01 22:13:02 +0530

    Refractor input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import './icon_card.dart';
+import './transparent_card.dart';
+
 const bottomButtonHeight = 80.0;
 const bottomButtonColor = Color(0xFFEA1556);
 const activeCardColor = Color(0xFF1D1F33);
@@ -74,48 +77,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconCard extends StatelessWidget {
-  final IconData iconData;
-  final String label;
 
-  IconCard({@required this.iconData, @required this.label});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          iconData,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        Text(
-          label,
-          style: TextStyle(color: Color(0xFFFDFFFF)),
-        )
-      ],
-    );
-  }
-}
 
-class TransparentCard extends StatelessWidget {
-  final Color color;
-  final Widget child;
-
-  TransparentCard({@required this.color, this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(14.0),
-      decoration: BoxDecoration(
-        color: this.color,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: this.child,
-    );
-  }
-}
