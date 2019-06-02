6e9fafcb6b7a6133656ff2fa7d18ec1c13a4a296
m-zaink <sidsmith9@gmail.com>
2019-06-02 14:41:03 +0530

    Update constants.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,7 +7,9 @@ const kBottomButtonColor = Color(0xFFEA1556);
 const kActiveCardColor = Color(0xFF1D1F33);
 const kInactiveCardColor = Color(0xFF191928);
 
-const kLabelTextStyle = TextStyle(color: Color(0xFFFDFFFF));
+const kLabelTextStyle = TextStyle(
+  color: Color(0xFFFDFFFF),
+);
 
 const kBottomButtonTextStyle = TextStyle(
   color: Color(0xFFFDFFFF),
@@ -20,6 +22,25 @@ const kNumberTextStyle = TextStyle(
   fontWeight: FontWeight.w900,
 );
 
+const kBmiNumberTextStyle = TextStyle(
+  fontSize: 70.0,
+  fontWeight: FontWeight.w900,
+);
+
 const kSubscriptTextStyle = TextStyle(
   fontSize: 8.0,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTitleTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: Colors.green,
+);
+
+const kResultSuggestionTextStyle = TextStyle(
+  fontSize: 20.0,
 );
